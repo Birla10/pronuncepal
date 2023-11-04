16d48f93ce7fa13235b494cfdc3acd6cd7d60218
LakshmaReddyCSWSU <chada.12@wright.edu>
2023-11-05 03:16:15 +0530

    navigation bar ui creation

---
+++ speech-ui/src/components/NavBar.js
@@ -0,0 +1,41 @@
+import Logo from '../images/logo.jpeg'
+import '../style/navBar.css'
+function NavBar(){
+    return(
+        <>
+         <nav className="navbar navbar-expand-lg">
+      
+      <div className="container-fluid">
+        <img className="logo" src={Logo} alt="Logo"/>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        
+            
+          </ul>
+          <form className="d-flex" role="search">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li >
+              Hi! UserName
+          </li>
+          <li >
+             Login
+          </li>
+          <li >
+              SignUp
+          </li>
+          </ul>
+          
+          
+            
+          </form>
+        </div>
+      </div>
+    </nav>
+        </>
+    )
+
+}
+export default NavBar;
