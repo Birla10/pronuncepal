16a257524baea2a17921c24b8051bc71603435c7
LakshmaReddyCSWSU <chada.12@wright.edu>
2023-11-08 22:07:33 +0530

    change in file name

--- speech-ui/src/App.js
+++ speech-ui/src/App.js
@@ -1,13 +1,17 @@
-
-import NavBar from './components/NavBar';
-import SignUp from './components/SignUp';
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import SignUp from './components/Signup';
 function App() {
   return (
     <>
-
-      <NavBar />
-      <SignUp />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+      </Routes>
+    </BrowserRouter>
   
     </>
   );
