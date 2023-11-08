bb1df2ade3b3f779f283d67703df838e1acc4aab
LakshmaReddyCSWSU <chada.12@wright.edu>
2023-11-08 22:42:51 +0530

    creation of UI pages

---
+++ speech-ui/src/components/UserHome.js
@@ -0,0 +1,37 @@
+import NavBar from "./NavBar";
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import Recording from "./Recording";
+import Practise from "./practise";
+import Analytics from "./Analytics";
+import History from "./History";
+import { useState } from "react";
+
+import "../style/about.css";
+
+function UserHome(){
+    const [key, setKey] = useState('practise');
+    return(
+        <>
+            <NavBar />
+            <Tabs
+      id="controlled-tab-example"
+      activeKey={key}
+      onSelect={(k) => setKey(k)}
+      className="mb-3"
+    >
+      <Tab eventKey="practise" title="Practise">
+        <Practise />
+      </Tab>
+      <Tab eventKey="analystics" title="Analytics">
+        <Analytics />
+      </Tab>
+      <Tab eventKey="history" title="History">
+        <History />
+      </Tab>
+      
+    </Tabs>
+        </>
+    )
+}
+export default UserHome;
