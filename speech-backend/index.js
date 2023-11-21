0402d495b3596437c465d3b3b69f807fde11d25a
Manideep M <mittapelly.4@wright.edu>
2023-11-21 17:41:09 -0500

    analyitcsBackend creation

--- speech-backend/index.js
+++ speech-backend/index.js
@@ -8,10 +8,14 @@ app.use(bodyParser.json());
 
 
 const loginRoute = require('./loginRoute');
+const analyticsLine = require('./analyticsLine');
+const analyticsBar = require('./analyticsBar');
 
 
 
 app.use('/login',loginRoute)
+app.use('/analyticsLine',analyticsLine)
+app.use('/analyticsBar',analyticsBar)
 
 app.listen(process.env.PORT,()=>{
     console.log("Listening on Port 8080")
