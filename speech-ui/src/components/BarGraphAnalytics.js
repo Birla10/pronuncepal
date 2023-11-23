1ab677a2b5ae95e36dee85236c19476fb60ae77c
Manideep M <mittapelly.4@wright.edu>
2023-11-22 23:15:22 -0500

    integration of analytics backend and frontend

--- speech-ui/src/components/BarGraphAnalytics.js
+++ speech-ui/src/components/BarGraphAnalytics.js
@@ -7,52 +7,9 @@ import {
     CartesianGrid,
     Bar
   } from "recharts";
-function BarGraphAnalytics() {
-    const data = [
-        {
-          name: 'Page A',
-          uv: 4000,
-          pv: 2400,
-          amt: 2400,
-        },
-        {
-          name: 'Page B',
-          uv: 3000,
-          pv: 1398,
-          amt: 2210,
-        },
-        {
-          name: 'Page C',
-          uv: 2000,
-          pv: 9800,
-          amt: 2290,
-        },
-        {
-          name: 'Page D',
-          uv: 2780,
-          pv: 3908,
-          amt: 2000,
-        },
-        {
-          name: 'Page E',
-          uv: 1890,
-          pv: 4800,
-          amt: 2181,
-        },
-        {
-          name: 'Page F',
-          uv: 2390,
-          pv: 3800,
-          amt: 2500,
-        },
-        {
-          name: 'Page G',
-          uv: 3490,
-          pv: 4300,
-          amt: 2100,
-        },
-      ];
+function BarGraphAnalytics({barData}) {
     
+   
     return (
         <>
             <br /><br />
@@ -61,7 +18,7 @@ function BarGraphAnalytics() {
             <BarChart
                 width={500}
                 height={300}
-                data={data}
+                data={barData}
                 margin={{
                     top: 5,
                     right: 30,
@@ -71,7 +28,7 @@ function BarGraphAnalytics() {
                 barSize={20}
             >
                 <XAxis
-                    dataKey="name"
+                    dataKey="dateUpload"
                     scale="point"
                     padding={{ left: 10, right: 10 }}
                 />
@@ -79,7 +36,7 @@ function BarGraphAnalytics() {
                 <Tooltip />
                 <Legend />
                 <CartesianGrid strokeDasharray="3 3" />
-                <Bar dataKey="uv" fill="#8884d8" background={{ fill: "#eee" }} />
+                <Bar dataKey="Number Of Tries" fill="#8884d8" background={{ fill: "#eee" }} />
             </BarChart>
         </>
     )
